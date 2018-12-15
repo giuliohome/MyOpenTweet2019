9c37b6b515aed0937d56266ddb2cb8526c4ca2fe
giuliohome <giuliohome@gmail.com>
2018-12-15 20:32:12 +0100

    fix for issue #1 (crash when phone is rotated)

--- app/src/main/java/com/wordpress/giuliohome/testviewmodel/data/Word.kt
+++ app/src/main/java/com/wordpress/giuliohome/testviewmodel/data/Word.kt
@@ -18,7 +18,7 @@ interface WordDao {
     @get:Query("SELECT * from word_table ORDER BY word ASC")
     val allWords: LiveData<List<Word>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(word: Word)
 
     @Query("DELETE FROM word_table")
